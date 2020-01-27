5cdfaf0d9662c5b454bd98339111ca8499ce27ed
Julian Uran <julianx@gmail.com>
2020-01-27 15:23:40 -0500

    Models: updating image statuses.

--- labelMeNav/models.py
+++ labelMeNav/models.py
@@ -4,29 +4,30 @@ from labelMeNav import constants
 
 
 class Images(models.Model):
-    name = models.CharField(max_length=200)
-    verified = models.BooleanField(default=False)
-    human_tagged = models.BooleanField(default=False)
-    model_tagged = models.BooleanField(default=False)
-    pending = models.BooleanField(default=True)
+    image_file_name = models.CharField(max_length=200)
+    human_stamps_complete = models.BooleanField(default=False)
+    human_pages_complete = models.BooleanField(default=False)
+    machine_stamps_complete = models.BooleanField(default=False)
+    machine_pages_complete = models.BooleanField(default=False)
 
     @property
-    def status(self):
-        if self.human_tagged:
-            return constants.HUMAN_TAGGED
-        elif self.model_tagged:
-            return constants.MODEL_TAGGED
-        elif self.verified:
-            return constants.VERIFIED
+    def pending(self):
+        return self.human_stamps_complete and self.human_pages_complete and \
+               self.machine_stamps_complete and self.machine_pages_complete
+
+    @property
+    def verified(self):
+        return not self.pending
+
+    @property
+    def status_color_rgb(self):
+        if self.human_stamps_complete:
+            return constants.HUMAN_STAMPS_COMPLETE
+        elif self.human_pages_complete:
+            return constants.HUMAN_PAGES_COMPLETE
+        elif self.machine_stamps_complete:
+            return constants.MACHINE_PAGES_COMPLETE
+        elif self.machine_pages_complete:
+            return constants.MACHINE_STAMPS_COMPLETE
         else:
             return constants.PENDING
-
-    def set_verified(self):
-        self.verified, self.pending = True, False
-
-    def set_model_tagged(self):
-        self.model_tagged, self.human_tagged, self.pending = True, False, True
-
-    def set_human_tagged(self):
-        self.model_tagged, self.human_tagged = False, True
-        self.set_verified()
